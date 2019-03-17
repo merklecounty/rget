22e9b2b73e872cd33d6f821aff0a38539c452c84
Brandon Philips <brandon.philips@coreos.com>
2019-03-17 06:07:24 +0000

    wip

--- go.mod
+++ go.mod
@@ -2,4 +2,10 @@ module github.com/philips/sget
 
 go 1.12
 
-require github.com/google/go-github/v24 v24.0.1
+require (
+	github.com/google/go-github/v24 v24.0.1
+	github.com/google/trillian v1.2.1
+	github.com/grpc-ecosystem/grpc-gateway v1.8.5 // indirect
+	go.opencensus.io v0.19.1 // indirect
+	google.golang.org/genproto v0.0.0-20190307195333-5fe7a883aa19 // indirect
+)
--- main.go
+++ main.go
@@ -2,9 +2,12 @@ package main
 
 import (
 	"context"
+	"encoding/hex"
 	"fmt"
 
 	"github.com/google/go-github/v24/github"
+	"github.com/google/trillian/merkle"
+	"github.com/google/trillian/merkle/rfc6962"
 )
 
 func main() {
@@ -16,9 +19,27 @@ func main() {
 
 	for _, r := range rs {
 		release := *r.TagName
+		println(release)
 		gt := *tags[0].GetCommit().SHA
+		println(gt)
 		for _, a := range r.Assets {
 			println(*a.URL)
 		}
 	}
+
+	t := merkle.NewInMemoryMerkleTree(rfc6962.DefaultHasher)
+	println(hex.EncodeToString(t.CurrentRoot().Hash()))
+	//t.AddLeaf([]byte(decodeHexStringOrPanic("")))
+	//t.AddLeaf([]byte(decodeHexStringOrPanic("00")))
+	//t.AddLeaf([]byte(decodeHexStringOrPanic("10")))
+	//println(hex.EncodeToString(t.CurrentRoot().Hash()))
+}
+
+func decodeHexStringOrPanic(hs string) []byte {
+	data, err := hex.DecodeString(hs)
+	if err != nil {
+		panic(fmt.Errorf("failed to decode test data: %s", hs))
+	}
+
+	return data
 }
