e2fb8781cdc3f282b32e912f8970de38c0d91c08
Brandon Philips <brandon.philips@coreos.com>
2019-03-17 06:53:20 +0000

    wip

--- main.go
+++ main.go
@@ -2,8 +2,11 @@ package main
 
 import (
 	"context"
+	"crypto/sha256"
 	"encoding/hex"
 	"fmt"
+	"io"
+	"os"
 
 	"github.com/google/go-github/v24/github"
 	"github.com/google/trillian/merkle"
@@ -11,17 +14,26 @@ import (
 )
 
 func main() {
-	fmt.Println("vim-go")
+	var (
+		release string
+		commit  string
+	)
+
+	if len(os.Args) < 2 {
+		fmt.Printf("usage: sget file file file\n")
+		os.Exit(1)
+	}
+
 	client := github.NewClient(nil)
 	opts := &github.ListOptions{Page: 0, PerPage: 1}
 	rs, _, _ := client.Repositories.ListReleases(context.Background(), "philips", "releases-test", opts)
 	tags, _, _ := client.Repositories.ListTags(context.Background(), "philips", "releases-test", opts)
 
 	for _, r := range rs {
-		release := *r.TagName
+		release = *r.TagName
+		commit = *tags[0].GetCommit().SHA
 		println(release)
-		gt := *tags[0].GetCommit().SHA
-		println(gt)
+		println(commit)
 		for _, a := range r.Assets {
 			println(*a.URL)
 		}
@@ -33,6 +45,35 @@ func main() {
 	//t.AddLeaf([]byte(decodeHexStringOrPanic("00")))
 	//t.AddLeaf([]byte(decodeHexStringOrPanic("10")))
 	//println(hex.EncodeToString(t.CurrentRoot().Hash()))
+
+	var sha256sums string
+	for _, name := range os.Args[1:] {
+		_, sum := hashFile(name)
+		sha256sums = sha256sums + fmt.Sprintf("%x  %s\n", sum, name)
+		t.AddLeaf(sum)
+	}
+
+	fmt.Println(sha256sums)
+
+	rh := t.CurrentRoot().Hash()
+	fmt.Printf("merkle root: %s\n", hex.EncodeToString(rh))
+	fmt.Printf("domain: %s.%s.%s.sget.philips.github.io.secured.dev", hex.EncodeToString(rh[:16]), hex.EncodeToString(rh[16:]), release)
+}
+
+func hashFile(name string) (err error, sum []byte) {
+	h := sha256.New()
+	file, err := os.Open(name)
+	if err != nil {
+		panic(fmt.Sprintf("%s: %v", name, err))
+	}
+
+	_, err = io.Copy(h, file)
+	if err != nil {
+		panic(err)
+	}
+	sum = h.Sum(nil)
+
+	return nil, sum
 }
 
 func decodeHexStringOrPanic(hs string) []byte {
