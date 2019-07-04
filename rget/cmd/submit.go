a4ee3c36b379198a52bf83ec58fe17b56c0eb63b
Brandon Philips <brandon.philips@coreos.com>
2019-07-03 22:38:20 -0700

    rget: submit: remove panics, cleanup doc strings

--- rget/cmd/submit.go
+++ rget/cmd/submit.go
@@ -15,9 +15,11 @@
 package cmd
 
 import (
+	"fmt"
 	"io/ioutil"
 	"net/http"
 	"net/url"
+	"os"
 
 	"github.com/merklecounty/rget/rgetwellknown"
 	"github.com/spf13/cobra"
@@ -25,29 +27,16 @@ import (
 
 // submitCmd represents the submit command
 var submitCmd = &cobra.Command{
-	Use:   "submit",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Use:   "submit [https://example.com/path/to/downloads/SHA256SUMS]",
+	Short: "submit a URL to the recorder",
+	Long: `The submitted URL will be fetched by the recorder service,
+a record domain name will be generated, and a subsequent request to that
+domain will cause a certificate to be generated and logged.`,
 	Run: submit,
 }
 
 func init() {
 	rootCmd.AddCommand(submitCmd)
-
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// submitCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// submitCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
 
 func submit(cmd *cobra.Command, args []string) {
@@ -56,11 +45,13 @@ func submit(cmd *cobra.Command, args []string) {
 	})
 	defer resp.Body.Close()
 	if err != nil {
-		panic(err)
+		fmt.Printf("submit POST error: %v\n", err)
+		os.Exit(1)
 	}
 
 	_, err = ioutil.ReadAll(resp.Body)
 	if err != nil {
-		panic(err)
+		fmt.Printf("submit read response error: %v\n", err)
+		os.Exit(1)
 	}
 }
