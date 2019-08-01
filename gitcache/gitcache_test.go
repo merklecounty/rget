0548172927cc4b069d1515269b68b7f81f79b042
Brandon Philips <brandon.philips@coreos.com>
2019-08-01 12:01:54 -0700

    gitcache: test GitCache directly
    Initialize a repo in the test to test the basic operations. This will
    greatly increase code coverage.

--- gitcache/gitcache_test.go
+++ gitcache/gitcache_test.go
@@ -8,28 +8,65 @@ import (
 	"reflect"
 	"testing"
 
-	"github.com/merklecounty/rget/autocert"
+	"gopkg.in/src-d/go-git.v4"
+	"gopkg.in/src-d/go-git.v4/plumbing/object"
 )
 
-func TestDirCache(t *testing.T) {
+func TestPrefix(t *testing.T) {
 	dir, err := ioutil.TempDir("", "autocert")
 	if err != nil {
 		t.Fatal(err)
 	}
 	defer os.RemoveAll(dir)
-	dir = filepath.Join(dir, "certs") // a nonexistent dir
-	cache := autocert.DirCache(dir)
+
+	url := filepath.Join(dir, "repo")
+	err = os.Mkdir(url, 0755)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	r, err := git.PlainInit(url, false)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	w, err := r.Worktree()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	err = ioutil.WriteFile(filepath.Join(url, "README"), []byte("Hello world"), 0755)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	_, err = w.Add("README")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	_, err = w.Commit("README\n", &git.CommitOptions{Author: &object.Signature{
+		Name: "Zohra",
+	}})
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	println(url)
+	gc, err := NewGitCache(filepath.Join(url, git.GitDirName), nil, filepath.Join(dir, "cache"))
+	if err != nil {
+		t.Fatal(err)
+	}
 	ctx := context.Background()
 
-	// test prefix
 	expected := []string{"dummy1", "dummy1.dummy1", "dummy1.dummy1.dummy1"}
 	for _, n := range expected {
-		if err := cache.Put(ctx, n, []byte{1}); err != nil {
+		if err := gc.Put(ctx, n, []byte{1}); err != nil {
 			t.Fatalf("put: %v", err)
 		}
 	}
 
-	matches, err := prefix(cache, "dummy")
+	matches, err := gc.Prefix(ctx, "dummy")
 	if err != nil {
 		t.Fatalf("prefix: %v", err)
 	}
@@ -37,5 +74,4 @@ func TestDirCache(t *testing.T) {
 	if !reflect.DeepEqual(matches, expected) {
 		t.Errorf("matches = %v; want %v", matches, expected)
 	}
-
 }
